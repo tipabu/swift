3f3502c6dfcbb78408fc9d7f48c984b9f5568201
Tim Burke <tim.burke@gmail.com>
2022-04-12 14:00:48 -0700

    systemd: Send STOPPING/RELOADING notifications
    See https://www.freedesktop.org/software/systemd/man/sd_notify.html#Description
    for more information.
    
    Note that this requires that we keep the NOTIFY_SOCKET env var
    around for more than just the first READY message, so we want to be
    careful about when we're sending the default "READY=1".
    
    UpgradeImpact
    =============
    Since prior versions of Swift would unset the NOTIFY_SOCKET env var,
    services must be fully restarted (rather than seamlessly reloaded) to
    emit the new messages.
    
    Related-Change: Ice224fc2a6ba0150be180955037c13fc90365479
    Change-Id: I201734ae0d6232ecb1923e67864dd928f90b6586

--- swift/common/utils.py
+++ swift/common/utils.py
@@ -6817,7 +6817,7 @@ def get_db_files(db_path):
     return sorted(results)
 
 
-def systemd_notify(logger=None):
+def systemd_notify(logger=None, msg=b"READY=1"):
     """
     Notify the service manager that started this process, if it is
     systemd-compatible, that this process correctly started. To do so,
@@ -6826,8 +6826,10 @@ def systemd_notify(logger=None):
     https://www.freedesktop.org/software/systemd/man/sd_notify.html
 
     :param logger: a logger object
+    :param msg: the message to send
     """
-    msg = b'READY=1'
+    if not isinstance(msg, bytes):
+        msg = msg.encode('utf8')
     notify_socket = os.getenv('NOTIFY_SOCKET')
     if notify_socket:
         if notify_socket.startswith('@'):
@@ -6838,7 +6840,6 @@ def systemd_notify(logger=None):
             try:
                 sock.connect(notify_socket)
                 sock.sendall(msg)
-                del os.environ['NOTIFY_SOCKET']
             except EnvironmentError:
                 if logger:
                     logger.debug("Systemd notification failed", exc_info=True)
